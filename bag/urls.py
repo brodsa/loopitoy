e93f7e275e989a4a36445557c2d11073e4f51fc3
brodsa <brodinova@gmail.com>
2024-06-16 11:34:04 +0200

    feat: imeplement the function to remove a toy from shopping bag - create view to remove toy from bag - create url - add button in shopping bag to remove the item

--- bag/urls.py
+++ bag/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import view_bag, add_to_bag
+from .views import (
+    view_bag,
+    add_to_bag,
+    remove_from_bag
+)
 
 
 urlpatterns = [
     path('', view_bag, name='view_bag'),
     path('add/<int:item_id>/', add_to_bag, name='add_to_bag'),
+    path('remove/<int:item_id>/', remove_from_bag, name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -18,18 +18,39 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """Add"""
-
+    """A view to add a toy into shopping bag"""
 
     toy = get_object_or_404(Toys, pk=int(item_id))
     redirect_url = request.POST.get('redirect_url')
 
     bag = request.session.get('bag', {})
     bag[item_id] = 1
-    messages.success(request, f'Added {toy.name} to your bag')
     toy_sold = Toys.objects.get(pk = int(item_id))
     toy_sold.status = 'in_bag'
     toy_sold.save()
+    messages.success(request, f'Added {toy.name} to your bag')
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+
+def remove_from_bag(request, item_id):
+    """A view to remove a toy from shopping bag"""
+    try:
+        toy = get_object_or_404(Toys, pk=int(item_id))
+        redirect_url = request.POST.get('redirect_url')
+
+        bag = request.session.get('bag', {})
+        del bag[f'{str(item_id)}']
+        toy_sold = Toys.objects.get(pk = int(item_id))
+        toy_sold.status = 'eshop'
+        toy_sold.save()
+        messages.success(request, f'Deleted {toy.name} to your bag')
+        
+        request.session['bag'] = bag
+        return redirect(redirect_url)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
