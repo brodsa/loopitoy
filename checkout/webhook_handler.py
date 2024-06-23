edce8c0fc590f8e58ffd7e58c1a90c271d78ec33
brodsa <brodinova@gmail.com>
2024-06-23 20:09:08 +0200

    feat: setup cache checkout data from meta data

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,17 +16,20 @@ class StripeWH_Handler:
             status=200)
     
     def handle_payment_intent_succeeded(self, event):
-            """
-            Handle the payment_intent.succeeded webhook from Stripe
-            """
-            return HttpResponse(
-                content=f'Webhook received: {event["type"]}',
-                status=200)
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        print('handle_success')
+        intent = event.data.object
+        print(intent)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
 
     def handle_payment_intent_payment_failed(self, event):
-            """
-            Handle the payment_intent.payment_failed webhook from Stripe
-            """
-            return HttpResponse(
-                content=f'Failed, Webhook received: {event["type"]}',
-                status=200)
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Failed, Webhook received: {event["type"]}',
+            status=200)
