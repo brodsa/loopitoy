d6cfb56c9c3de31ed19478873fccabef0a8b00b7
Sarka Grubner <brodinova@gmail.com>
2024-08-07 15:55:26 +0000

    fix: remove newsleter app

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,23 +1,2 @@
-from django import forms
-from .models import Subscription
 
-
-class AddSubscription(forms.ModelForm):
-    """Form for adding email to newsletter subscribers"""
-
-    class Meta:
-        """Meta class"""
-
-        model = Subscription
-        fields = [
-            "email",
-        ]
-
-        widgets = {
-            "email": forms.TextInput(attrs={"id": "newsletter_email"})
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        field.widget.attrs['label'] = "Enter email"
         
--- newsletter/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from newsletter.views import SubscribeToNewsletter
-
-urlpatterns = [
-    path("", SubscribeToNewsletter.as_view(), name="subscribe_to_newsletter"),
-]
